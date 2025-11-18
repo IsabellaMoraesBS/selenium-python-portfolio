a77de4ebe3bcfd3f670619136662931ebeaa6b98
Isabella Moraes <isabella.mbs@outlook.com>
2025-11-18 15:39:19 -0300

    Update driver_manager.py
    Comentários

--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -3,15 +3,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
+# Classe para gerenciar a criação do WebDriver do Chrome
 class DriverManager:
+    # param headless: Se True, roda o navegador em modo headless (sem interface gráfica)
     def __init__(self, headless=False):
         self.headless = headless
 
+    # Inicializa e retorna uma instância do Chrome WebDriver configurada
     def get_driver(self):
         options = Options()
         if self.headless:
             options.add_argument('--headless=new')
+            
+        # Configura o serviço do ChromeDriver usando webdriver_manager
         service = Service(ChromeDriverManager().install())
+
+        # Cria a instância do WebDriver
         driver = webdriver.Chrome(service=service, options=options)
+
+        # Define espera implícita de 5 segundos para encontrar elementos
         driver.implicitly_wait(5)
+        
         return driver
