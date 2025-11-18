f67b732bb2947ec29b627d3fbff43d65e3624699
Isabella Moraes <isabella.mbs@outlook.com>
2025-11-18 15:41:47 -0300

    Update conftest.py
    Comentários

--- conftest.py
+++ conftest.py
@@ -3,7 +3,14 @@ from utils.driver_manager import DriverManager
 
 @pytest.fixture
 def driver():
+    # cria o manager em modo headless
     dm = DriverManager(headless=True)
+
+    # inicializa o Chrome
     d = dm.get_driver()
+
+    # entrega o driver ao teste
     yield d
+
+    # fecha o navegador após o teste
     d.quit()
